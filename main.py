212de2b4783183a5d728affb88ab3d9b8251fd11
Walker Reynolds <walkerreynolds16@gmail.com>
2019-12-23 01:57:09 -0500

    These specials are going to kill me

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def downloadMovies():
         newDirPath = JIMS_PLEX_MOVIE_PATH + fileTitle + '/'
         # print("New Dir Path = {}".format(newDirPath))
 
-        newFilePath = newDirPath + fileName.replace('+','_').replace('!','_').replace("–","___").replace("】", "___").replace("【", "___")
+        newFilePath = newDirPath + fileName.replace('+','_').replace('!','_').replace("–","___").replace("】", "___").replace("【", "___").replace("é", "__")
         # print("New File Path = {}".format(newFilePath))
     
 
@@ -103,7 +103,7 @@ def downloadTVShows():
                 # print("{} - {} - {}".format(episodesResponseDict['MediaContainer']['@title1'], seasonTitle, episode['@title']))
                 episodeTitle = episode['@title']
                 episodeFileName = utils.getFileNameFromPath(episode['Media']['Part']['@file'])
-                episodePath = seasonDirectory + episodeFileName.replace('+','_').replace('!','_').replace("–","___").replace("】", "___").replace("【", "___")
+                episodePath = seasonDirectory + episodeFileName.replace('+','_').replace('!','_').replace("–","___").replace("】", "___").replace("【", "___").replace("é", "__")
                 formattedFileSize = utils.formatSizeInteger(int(episode['Media']['Part']['@size']))
                 mediaKey = episode['Media']['Part']['@key']
 
